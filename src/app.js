e2ce4184b66b6b7a8d123eb15083d033c2ceaa38
Beta-23 <alfred.dominguez@gmail.com>
2020-06-23 23:40:26 -0700

    Implement React-Router-Dom library

--- src/app.js
+++ src/app.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'; 
-ReactDOM.render(<p>New Expensify App Boilerplate</p>,document.getElementById('app'));
+
+const ExpenseDashboardPage = () => (
+    <div>
+        App dashboard Component
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        App Expense Component
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} />
+            <Route path="/create" component={AddExpensePage}/>
+        </div>
+    </BrowserRouter>
+)
+
+
+ReactDOM.render(routes,document.getElementById('app'));
 
 
 
