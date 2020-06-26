c0594aa3f5e267c610ae4bf4dae97a94186c85d1
Beta-23 <alfred.dominguez@gmail.com>
2020-06-26 16:28:01 -0700

    Update NotFoundPage with Link component

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Functional Components
 const NotFoundPage = () => (
     <div>
-        404 
+        404 | <Link to="/">Go Home</Link>
     </div>
 );
 
