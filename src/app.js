3b62973e249121a0c0ec490304fd08e99d0e7e0c
Beta-23 <alfred.dominguez@gmail.com>
2020-06-25 23:16:28 -0700

    Test switch router

--- src/app.js
+++ src/app.js
@@ -37,13 +37,13 @@ const NotFoundPage = () => (
 
 const routes = (
     <BrowserRouter>
-        <Switch>
+        <div>
             <Route path="/" component={ ExpenseDashboardPage } exact={ true }/>
             <Route path="/create" component={ AddExpensePage } />
             <Route path="/edit" component={ EditExpensePage } />
             <Route path="/help" component={ HelpPage } />
             <Route component={ NotFoundPage } />
-        </Switch>
+        </div>
     </BrowserRouter>
 )
 
