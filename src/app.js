b3dfb67357669bc7d22d6b0dcca2800062182615
Beta-23 <alfred.dominguez@gmail.com>
2020-06-25 23:17:56 -0700

    Fix router issues

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'; 
 
@@ -37,13 +37,13 @@ const errorPage = () => (
 
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={ ExpenseDashboardPage } exact={ true }/>
             <Route path="/create" component={ AddExpensePage } />
             <Route path="/edit" component={ EditExpensePage } />
             <Route path="/help" component={ HelpPage } />
             <Route path="/404" component={ errorPage } />
-        </div>
+        </Switch>
     </BrowserRouter>
 )
 
