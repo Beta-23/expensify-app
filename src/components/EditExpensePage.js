b0d62921b1e9081fc7872178170669055c7301dd
Beta-23 <alfred.dominguez@gmail.com>
2020-06-26 17:06:09 -0700

    Test the react router props obj and query search

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        <h5>EDIT | App Expense Component</h5>
-    </div>
-);
+const EditExpensePage = (props) => {
+    console.log(props)
+    return (
+        <div>
+            Editing expense with id of {props.match.params.id}
+        </div>
+    );
+};
 
 export default EditExpensePage;
