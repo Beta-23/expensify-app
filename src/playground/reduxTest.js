c0cbb742000d05c1c9bf5b4c64477f4b43925792
Beta-23 <alfred.dominguez@gmail.com>
2020-07-03 17:41:36 -0700

    Change redux version

--- src/playground/reduxTest.js
+++ src/playground/reduxTest.js
@@ -3,7 +3,7 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
     if (action.type === 'INCREMENT') {
         return {
-            count: state.count + 1
+            count: ++state.count
         };
     } else {
         return state;
