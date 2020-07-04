10319416667acfac5bc7514aea89bd129dcc6e97
Beta-23 <alfred.dominguez@gmail.com>
2020-07-03 17:32:21 -0700

    Test action type to increment count

--- src/playground/reduxTest.js
+++ src/playground/reduxTest.js
@@ -1,7 +1,26 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 10 }) => {
-    return state;
+const store = createStore((state = { count: 0 }, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            count: state.count + 1
+        };
+    } else {
+        return state;
+    }
 });
 
 console.log(store.getState());
+
+/* Actions - is an Object that gets sent to the store, 
+it describes the action type to dispatch to state. i.e increment, decrement, reset */
+
+// increment the count
+store.dispatch({
+    type: 'INCREMENT',
+});
+
+// increment the count
+store.dispatch({
+    type: 'INCREMENT',
+});
