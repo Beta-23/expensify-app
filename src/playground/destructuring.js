43e30b182bca22eef2b641ad1aa324f470f3d52f
Beta-23 <alfred.dominguez@gmail.com>
2020-07-06 15:01:28 -0700

    Test the array destructuring function

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,34 +1,42 @@
-// destructuring the person obj
-const person = {
-    name: 'Andy',
-    age: 21,
-    location: {
-        city: 'NYC',
-        temp: 70
-    }
-};
+// OBJECT: destructuring the person obj
+// const person = {
+//     name: 'Andy',
+//     age: 21,
+//     location: {
+//         city: 'NYC',
+//         temp: 70
+//     }
+// };
 
 // grabing the name and age from the person obj and default value
-const {name: firstName = 'Anonymous', age} = person;
-console.log(`${firstName} is ${age} old.`);
+// const {name: firstName = 'Anonymous', age} = person;
+// console.log(`${firstName} is ${age} old.`);
 
 // grabing the city and temp property from the nested location obj
-const {city, temp: temperature} =  person.location
+// const {city, temp: temperature} =  person.location
 
-if (city && temperature) {
-    console.log(`It's ${temperature} in ${city}.`);
-}
+// if (city && temperature) {
+//     console.log(`It's ${temperature} in ${city}.`);
+// }
 
 // destructuring the book obj
-const book = {
-    title: 'Envy is the worst',
-    author: 'John Doe',
-    publisher: {
-        name: 'Penguin',
-        year: 1999
-    }
-};
+// const book = {
+//     title: 'Envy is the worst',
+//     author: 'John Doe',
+//     publisher: {
+//         name: 'Penguin',
+//         year: 1999
+//     }
+// };
 
-const {name: publisherName = 'Self-Published'} = book.publisher;
+// const {name: publisherName = 'Self-Published'} = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+// ARRAY DESTRUCTURING:
+
+const address = [ '199 W. June Street', 'New York City', 'New York', '11111' ];
+
+// destructuring the array and variable assigment (,skip zip)
+const [ street, city, state ] = address;
+console.log(`You are in, ${city}, ${state}.`);
