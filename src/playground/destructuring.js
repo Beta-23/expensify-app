be7f95b8007e91fb0e0a387881fe9f4f5df09124
Beta-23 <alfred.dominguez@gmail.com>
2020-07-06 12:05:26 -0700

    Test ES6 Obj destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -9,15 +9,15 @@ const person = {
 };
 
 // grabing the name and age from the person obj and default value
-// const {name: firstName = 'Anonymous', age} = person;
-// console.log(`${firstName} is ${age} old.`);
+const {name: firstName = 'Anonymous', age} = person;
+console.log(`${firstName} is ${age} old.`);
 
 // grabing the city and temp property from the nested location obj
-// const {city, temp: temperature} =  person.location
+const {city, temp: temperature} =  person.location
 
-// if (city && temperature) {
-//     console.log(`It's ${temperature} in ${city}.`);
-// }
+if (city && temperature) {
+    console.log(`It's ${temperature} in ${city}.`);
+}
 
 // destructuring the book obj
 const book = {
