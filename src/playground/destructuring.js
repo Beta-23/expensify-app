571ef0de55bec95d5376a0d5543a6a6ca6c37f48
Beta-23 <alfred.dominguez@gmail.com>
2020-07-06 11:46:44 -0700

    reassign the temp property in obj

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,1 +1,20 @@
-console.log('destructuring is running');
+// destructuring the person obj
+const person = {
+    name: 'Andy',
+    age: 21,
+    location: {
+        city: 'NYC',
+        temp: 70
+    }
+};
+
+// grabing the name and age from the person obj
+const {name, age} = person;
+console.log(`${name} is ${age} old.`);
+
+// grabing the city and temp property from the nested location obj
+const {city, temp: temperature} =  person.location
+
+if (city && temperature) {
+    console.log(`It's ${temperature} in ${city}.`);
+}
