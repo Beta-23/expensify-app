0d357939d66755cbc57b1f369bc8e18b419b5669
Beta-23 <alfred.dominguez@gmail.com>
2020-07-06 12:01:31 -0700

    Create the book obj and destructure the publisher

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -9,12 +9,26 @@ const person = {
 };
 
 // grabing the name and age from the person obj and default value
-const {name: firstName = 'Anonymous', age} = person;
-console.log(`${firstName} is ${age} old.`);
+// const {name: firstName = 'Anonymous', age} = person;
+// console.log(`${firstName} is ${age} old.`);
 
 // grabing the city and temp property from the nested location obj
-const {city, temp: temperature} =  person.location
+// const {city, temp: temperature} =  person.location
 
-if (city && temperature) {
-    console.log(`It's ${temperature} in ${city}.`);
-}
+// if (city && temperature) {
+//     console.log(`It's ${temperature} in ${city}.`);
+// }
+
+// destructuring the book obj
+const book = {
+    title: 'Envy is the worst',
+    author: 'John Doe',
+    publisher: {
+        name: 'Penguin',
+        year: 1999
+    }
+};
+
+const {name: publisherName = 'Self-Published'} = book.publisher;
+
+console.log(publisherName);
