3cf3b10d14cb8bb889773522f11ff5c6da621c12
Beta-23 <alfred.dominguez@gmail.com>
2020-07-06 22:20:50 -0700

    Update comments

--- src/playground/reduxTest.js
+++ src/playground/reduxTest.js
@@ -22,8 +22,12 @@ const setCount = ({ count }) => ({
 const resetCount = () => ({
 	type : 'RESET'
 });
-// Store
-const store = createStore((state = { count: 0 }, action) => {
+
+/* Reducers
+    1. Reducers are pure functions, take in state and an action
+    2. Never change state or action only return new state obj */
+
+const countReducer = (state = { count: 0 }, action) => {
 	switch (action.type) {
 		case 'INCREMENT':
 			return {
@@ -46,7 +50,10 @@ const store = createStore((state = { count: 0 }, action) => {
 		default:
 			return state;
 	}
-});
+};
+
+// Store
+const store = createStore(countReducer);
 
 store.subscribe(() => {
 	console.log(store.getState());
