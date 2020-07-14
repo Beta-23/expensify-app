6b520d1ee904927b682942c326e3e0428c6a5812
Beta-23 <alfred.dominguez@gmail.com>
2020-07-13 23:34:32 -0700

    Integrate the new redux store with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,27 @@ const path = require('path');
 // entry -> output
 
 module.exports = {
-	entry     : './src/playground/redux-expensify.js',
-	output    : {
-		path     : path.join(__dirname, 'public'),
-		filename : 'bundle.js'
+	entry: './src/app.js',
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js'
 	},
-	module    : {
-		rules : [
+	module: {
+		rules: [
 			{
-				loader  : 'babel-loader',
-				test    : /\.js$/,
-				exclude : /node_modules/
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
 			},
 			{
-				test : /\.s?css$/,
-				use  : [ 'style-loader', 'css-loader', 'sass-loader' ]
+				test: /\.s?css$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
 			}
 		]
 	},
-	devtool   : 'eval-cheap-module-source-map',
-	devServer : {
-		contentBase        : path.join(__dirname, 'public'),
-		historyApiFallback : true
+	devtool: 'eval-cheap-module-source-map',
+	devServer: {
+		contentBase: path.join(__dirname, 'public'),
+		historyApiFallback: true
 	}
 };
