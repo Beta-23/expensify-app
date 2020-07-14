535deb50a57feff80f1a4e448d65ffc8dd06acc2
Beta-23 <alfred.dominguez@gmail.com>
2020-07-13 23:39:09 -0700

    Implement the redux store default state with react component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import storeconfig from './store/storeconfig';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss'; 
+import './styles/styles.scss';
 
-ReactDOM.render(<AppRouter />,document.getElementById('app'));
+const store = storeconfig();
 
+console.log(store.getState());
 
-
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
