f8437fcc7ef01856ed3b2050a327a9dd6a36f21b
Beta-23 <alfred.dominguez@gmail.com>
2020-07-19 22:25:31 -0700

    Connect the remove action to redux-store

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {removeExpense} from '../actions/expenses';
 
 // statless functional component
-const ExpenseListItem = ({description, amount, createdAt}) => (
+const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
 	<div>
 		<h3>{description}</h3>
 
 		<p>
 			Amount: {amount} Time Stamp: {createdAt}
+			<button
+				onClick={(e) => {
+					dispatch(removeExpense({id}));
+				}}
+			>
+				Remove
+			</button>
 		</p>
 	</div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
