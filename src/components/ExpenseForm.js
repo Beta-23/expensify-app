e92a17d612f7f6653046f4d2131037551b7b282f
Beta-23 <alfred.dominguez@gmail.com>
2020-07-22 12:20:23 -0700

    Create regular expression to check number and decimalfor amount input

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,17 +3,26 @@ import React from 'react';
 export default class ExpenseForm extends React.Component {
 	state = {
 		description: '',
-		note: ''
+		note: '',
+		amount: ''
 	};
+	// Event listeners
 	onDescriptionChange = (e) => {
 		const description = e.target.value;
 		this.setState(() => ({description}));
 	};
 
-	// used e.persist
+	// used e.persist event listeners
 	onNoteChange = (e) => {
 		e.persist();
 		this.setState(() => ({note: e.target.value}));
+	};
+	// Logic with regular expressions
+	onAmountChange = (e) => {
+		const amount = e.target.value;
+		if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+			this.setState(() => ({amount}));
+		}
 	};
 
 	render () {
@@ -27,7 +36,7 @@ export default class ExpenseForm extends React.Component {
 						value={this.state.description}
 						onChange={this.onDescriptionChange}
 					/>
-					<input type='number' placeholder='Amount' />
+					<input type='text' placeholder='Amount' value={this.state.amount} onChange={this.onAmountChange} />
 					<textarea placeholder='Add Expense Notes (optional)' value={this.state.note} onChange={this.onNoteChange} />
 					<button>Add Expense</button>
 				</form>
