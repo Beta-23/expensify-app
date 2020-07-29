faf23c6bf85ad4bb434ed825a9a89b967e2752f8
Beta-23 <alfred.dominguez@gmail.com>
2020-07-28 23:13:29 -0700

    Update code comments

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -11,7 +11,7 @@ const AddExpensePage = (props) => (
 		<ExpenseForm
 			onSubmit={(expense) => {
 				props.dispatch(addExpense(expense));
-				props.history.push('/');
+				props.history.push('/'); // Use the history method to push data to dashboard
 			}}
 		/>
 	</div>
