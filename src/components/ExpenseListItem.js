2e09e331cb62e480daa08f65a6ce919cdcaa03e9
Beta-23 <alfred.dominguez@gmail.com>
2020-07-30 14:12:40 -0700

    Test removeExpense dispatch

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 // statless functional component
-const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
+const ExpenseListItem = ({id, description, amount, createdAt}) => (
 	<div>
 		<Link to={`/edit/${id}`} activeClassName='is-active' exact={true}>
 			<h3>{description}</h3>
 		</Link>
 		<p>
-			Amount: {amount} Time Stamp: {createdAt}
+			Amount: {amount} - {createdAt}
 		</p>
 	</div>
 );
