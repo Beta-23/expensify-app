83d633704c8c533ddb153b705f04deb8bc81314a
Beta-23 <alfred.dominguez@gmail.com>
2020-08-03 23:13:35 -0700

    Create the first jest test assertion

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,5 +1,11 @@
 const add = (a, b) => a + b;
 
+// Passing in two arguments, string and arrow function with test cases
 test('should add two numbers', () => {
 	const result = add(3, 4);
+
+	//assertion
+	if (result !== 7) {
+		throw new Error(`You added 4 and 3 result was ${result}. Expected 7`);
+	}
 });
