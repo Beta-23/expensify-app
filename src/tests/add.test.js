77f6e58f1ce46aedc21f4502bc761c681e1c15cb
Beta-23 <alfred.dominguez@gmail.com>
2020-08-03 23:59:27 -0700

    Test no name greeting

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,6 +1,6 @@
 const add = (a, b) => a + b;
 
-const generalGreeting = (name) => `Hello ${name}!`;
+const generalGreeting = (name = 'Anonymous') => `Hello ${name}!`;
 // Passing in two arguments, string and arrow function with test cases
 test('should add two numbers', () => {
 	const result = add(3, 4);
@@ -10,4 +10,9 @@ test('should add two numbers', () => {
 test('should return greeting from name', () => {
 	const result = generalGreeting('Jordan');
 	expect(result).toBe('Hello Jordan!');
+});
+
+test('should return greeting for no name', () => {
+	const result = generalGreeting();
+	expect(result).toBe('Hello Anonymous!');
 });
