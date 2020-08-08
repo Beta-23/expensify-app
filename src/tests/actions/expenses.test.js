ed4a176a04473675c11c9f90ae3ff36ce81b11eb
Beta-23 <alfred.dominguez@gmail.com>
2020-08-07 23:39:04 -0700

    Test edit expense action object

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,4 @@
-import {removeExpense} from '../../actions/expenses';
+import {removeExpense, editExpense} from '../../actions/expenses';
 
 // Remove Expenses
 test('Test should remove expense action object', () => {
@@ -6,5 +6,15 @@ test('Test should remove expense action object', () => {
 	expect(action).toEqual({
 		type: 'REMOVE_EXPENSE',
 		id: 'Fun123'
+	}); //assertion
+});
+
+// Edit Expenses
+test('Test should setup edit expense action object', () => {
+	const action = editExpense('Fun123', {note: 'New note value'});
+	expect(action).toEqual({
+		type: 'EDIT_EXPENSE',
+		id: 'Fun123',
+		updates: {note: 'New note value'}
 	}); //assertion
 });
