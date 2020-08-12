c1192999871dafbd18fd169d4b4cdfbe79432d78
Beta-23 <alfred.dominguez@gmail.com>
2020-08-11 23:39:02 -0700

    Test should generate set start date action object

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,10 @@
+import moment from 'moment';
+import {setStartDate, setEndDate} from '../../actions/filters';
+
+test('Test should generate set start date action object', () => {
+	const action = setStartDate(moment(0));
+	expect(action).toEqual({
+		type: 'SET_START_DATE',
+		startDate: moment(0)
+	});
+}); //assertion
