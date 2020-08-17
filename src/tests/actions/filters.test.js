e657fff3cb9e384e3b5e36107aeeb014c9e5870e
Beta-23 <alfred.dominguez@gmail.com>
2020-08-16 22:01:33 -0700

    Test should generate set sort by date action object

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import {setStartDate, setEndDate, setTextFilter} from '../../actions/filters';
+import {setStartDate, setEndDate, setTextFilter, sortByDate} from '../../actions/filters';
 
 test('Test should generate set start date action object', () => {
 	const action = setStartDate(moment(0));
@@ -31,5 +31,11 @@ test('Test should generate set text filter action object with default value', ()
 	expect(action).toEqual({
 		type: 'SET_TEXT_FILTER',
 		text: ''
+	});
+}); //assertion
+
+test('Test should generate set sort by date action object', () => {
+	expect(sortByDate()).toEqual({
+		type: 'SORT_BY_DATE'
 	});
 }); //assertion
