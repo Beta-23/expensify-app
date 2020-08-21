421a8a34a7150a149f8cbdc62e4f4523a98df221
Beta-23 <alfred.dominguez@gmail.com>
2020-08-20 22:49:51 -0700

    Refactor sortBy date for reducer

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -16,7 +16,15 @@ test('Test should setup sortBy to amount', () => {
 	expect(state.sortBy).toBe('amount');
 }); //assertion
 
-test('Test should setup sortBy date', () => {
-	const state = filtersReducer(undefined, {type: 'SORT_BY_DATE'});
+test('Test should setup sortBy to date', () => {
+	const currentState = {
+		text: '',
+		startDate: undefined,
+		endDate: undefined,
+		sortBy: 'amount'
+	};
+
+	const action = {type: 'SORT_BY_DATE'};
+	const state = filtersReducer(currentState, action);
 	expect(state.sortBy).toBe('date');
 }); //assertion
