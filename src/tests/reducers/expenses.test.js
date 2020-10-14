03caff05859dd6bdbbd2c1604795a6d63ad0f1dd
Beta-23 <alfred.dominguez@gmail.com>
2020-10-13 22:17:56 -0700

    Test expenses reducer should remove expense by id

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -5,3 +5,12 @@ test('should set default state', () => {
 	const state = expensesReducer(undefined, {type: '@@INIT'});
 	expect(state).toEqual([]);
 }); //assertion expect empty state array
+
+test('should remove expense by id', () => {
+	const action = {
+		type: 'REMOVE_EXPENSE',
+		id: expenses[1].id
+	};
+	const state = expensesReducer(expenses, action);
+	expect(state).toEqual([expenses[0], expenses[2]]);
+}); //assertion to remove index 1 in array data
