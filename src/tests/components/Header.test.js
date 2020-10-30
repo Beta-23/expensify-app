b9ea5e0e784075b157fa8b1ac2c7726660d5fdb4
Beta-23 <alfred.dominguez@gmail.com>
2020-10-29 23:10:37 -0700

    Implement the snapshot jest test to validate header component renders correctly

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -5,5 +5,5 @@ import Header from '../../components/Header';
 test('should render Header correctly', () => {
     const renderer = new ReactShallowRenderer();
     renderer.render(<Header />);
-    console.log(renderer.getRenderOutput())
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
