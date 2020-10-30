9382a721b04b910a88a43b023379749a50356f5e
Beta-23 <alfred.dominguez@gmail.com>
2020-10-29 23:02:02 -0700

    Test Header obj and print obj

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -2,3 +2,8 @@ import React from 'react';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
 
+test('should render Header correctly', () => {
+    const renderer = new ReactShallowRenderer();
+    renderer.render(<Header />);
+    console.log(renderer.getRenderOutput())
+});
