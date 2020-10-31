207910d361852cc5c63dbdbf287714762f0b1288
Beta-23 <alfred.dominguez@gmail.com>
2020-10-30 22:13:54 -0700

    Implement enzyme toJSON to only display json snapshot

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme'
+import toJSON from 'enzyme-to-json'
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper).toMatchSnapshot();
-    
+    expect(toJSON(wrapper)).toMatchSnapshot();
+
     // const renderer = new ReactShallowRenderer();
     // renderer.render(<Header />);
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
