f545a9ce647a3ed147ef09c624fa2896d3d50233
Beta-23 <alfred.dominguez@gmail.com>
2020-10-30 22:07:16 -0700

    Test Snapshot with enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -4,7 +4,8 @@ import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.find('h1').text()).toBe('ExpensifyApp');
+    expect(wrapper).toMatchSnapshot();
+    
     // const renderer = new ReactShallowRenderer();
     // renderer.render(<Header />);
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
