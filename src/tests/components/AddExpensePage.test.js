869cc3fbc7a69bb3c2fdabd27fd961a1fc4ca982
Beta-23 <alfred.dominguez@gmail.com>
2020-11-13 21:18:08 -0800

    Test should handle onSubmit

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -7,7 +7,15 @@ test('should render AddExpensePage correctly', () => {
     const onSubmit = jest.fn();
     const history = { push: jest.fn() };
     const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
-    expect(wrapper).toMatchSnapshot();
-    
+    expect(wrapper).toMatchSnapshot(); 
+});
+
+test('should handle onSubmit', () => {
+    const onSubmit = jest.fn();
+    const history = { push: jest.fn() };
+    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
+    expect(history.push).toHaveBeenLastCalledWith('/');
+    expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
 });
 
