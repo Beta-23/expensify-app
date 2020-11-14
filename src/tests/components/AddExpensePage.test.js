9f105a5aee4d06e9c74fa0d35f35861df49b00e9
Beta-23 <alfred.dominguez@gmail.com>
2020-11-13 21:07:29 -0800

    Test should render AddExpensePage correctly

---
+++ src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { AddExpensePage } from '../../components/AddExpensePage';
+import expenses from '../fixtures/expenses';
+
+test('should render AddExpensePage correctly', () => {
+    const onSubmit = jest.fn();
+    const history = { push: jest.fn() };
+    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+    expect(wrapper).toMatchSnapshot();
+    
+});
+
