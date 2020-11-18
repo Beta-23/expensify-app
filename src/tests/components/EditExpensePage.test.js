2a500fc291ce13197a933c4e2652350001f9ef19
Beta-23 <alfred.dominguez@gmail.com>
2020-11-17 22:31:13 -0800

    Test should handle editExpense

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,20 +3,30 @@ import { shallow } from 'enzyme';
 import expenses from '../fixtures/expenses';
 import { EditExpensePage } from '../../components/EditExpensePage';
 
-let editExpense, wrapper;
+let editExpense, removeExpense, history, wrapper;
 
 beforeEach(() => {
     editExpense = jest.fn();
-    
+    removeExpense = jest.fn();
+    history = { push: jest.fn() };
     wrapper = shallow(
         <EditExpensePage
             editExpense={editExpense}
+            removeExpense={removeExpense}
+            history={history}
+            expense={expenses[2]}
         />
     );
 });
 
 test('should render EditExpensePage', () => {
     expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle editExpense', () => {
+    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2]);
+    expect(history.push).toHaveBeenLastCalledWith('/');
+    expect(editExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
 });
 
 
