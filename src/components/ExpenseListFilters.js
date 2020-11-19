049de3e855c59fb6879739fa081087786d33c672
Beta-23 <alfred.dominguez@gmail.com>
2020-11-18 20:34:45 -0800

    Refactor events into class methods for ExpenseListFilters

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -14,26 +14,28 @@ class ExpenseListFilters extends React.Component {
 	onFocusChange = (calendarFocused) => {
 		this.setState(() => ({calendarFocused}));
 	};
+	onTextChange = (e) => {
+		this.props.dispatch(setTextFilter(e.target.value));
+	};
+	onSortChange = (e) => {
+		if (e.target.value === 'date') {
+			this.props.dispatch(sortByDate());
+		}
+		else if (e.target.value === 'amount') {
+			this.props.dispatch(sortByAmount());
+		}
+	};
 	render () {
 		return (
 			<div>
 				<input
 					type='text'
 					value={this.props.filters.text}
-					onChange={(e) => {
-						this.props.dispatch(setTextFilter(e.target.value));
-					}}
+					onChange={this.onTextChange}
 				/>
 				<select
 					value={this.props.filters.sortBy}
-					onChange={(e) => {
-						if (e.target.value === 'date') {
-							this.props.dispatch(sortByDate());
-						}
-						else if (e.target.value === 'amount') {
-							this.props.dispatch(sortByAmount());
-						}
-					}}
+					onChange={this.onSortChange}
 				>
 					<option value='date'>Date</option>
 					<option value='amount'>Amount</option>
