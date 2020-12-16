edb031c8cbb8fdb25c8b776d2514841e1759c949
Beta-23 <alfred.dominguez@gmail.com>
2020-12-15 21:48:58 -0800

    Update node server for production

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const express = require('express');
+// Create express app
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 
+// Use public directory for public assets
 app.use(express.static(publicPath));
 
+// Res with index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
